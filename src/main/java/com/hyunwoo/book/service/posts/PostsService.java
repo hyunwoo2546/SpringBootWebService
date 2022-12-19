05648200e2b9df403dcebcc45284cbed2c82d6bf
Hyunwoo <hyunwoo2546@naver.com>
2022-12-19 20:37:06 +0900

    12/19 ver2

--- src/main/java/com/hyunwoo/book/service/posts/PostsService.java
+++ src/main/java/com/hyunwoo/book/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.hyunwoo.book.service.posts;
 
 import com.hyunwoo.book.domain.posts.Posts;
 import com.hyunwoo.book.domain.posts.PostsRepository;
+import com.hyunwoo.book.web.dto.PostsListResponseDto;
 import com.hyunwoo.book.web.dto.PostsResponseDto;
 import com.hyunwoo.book.web.dto.PostsSaveRequestDto;
 import com.hyunwoo.book.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,6 +37,13 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없읍니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
 }
