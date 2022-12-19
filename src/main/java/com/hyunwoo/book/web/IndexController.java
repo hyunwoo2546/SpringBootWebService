a6d0e56c4742d7f415f7dc24b488a52046eb5c11
Hyunwoo <hyunwoo2546@naver.com>
2022-12-20 02:28:35 +0900

    12/20 ver.05

--- src/main/java/com/hyunwoo/book/web/IndexController.java
+++ src/main/java/com/hyunwoo/book/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.hyunwoo.book.web;
 
+import com.hyunwoo.book.config.auth.dto.SessionUser;
 import com.hyunwoo.book.service.posts.PostsService;
 import com.hyunwoo.book.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -7,17 +8,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     /*# 전체 페이지 목록*/
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
